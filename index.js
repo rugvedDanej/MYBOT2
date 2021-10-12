27938a76b8be6a4259ce77fa64f6bc3af0394b41
GAME TECH X <90027959+rugvedDanej@users.noreply.github.com>
2021-10-12 15:50:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = process.env.prefix;
+
+cclien.once('ready', () => {
+      console.log(client.user.tag + " has logged in!")
+});
+
+client.on('message', () => {
+        if(message.content === prefix + "ping") {
+        message.reply('Pong!')
+        }
+});
+
+client.login(process.env.token)
